ca17239ef2eb214bc79ee2aac97031e79d2ae350
Bill Liu <billliu@BilldeMacBook-Pro.local>
2024-07-05 16:18:17 +0800

    inferece

--- inference.py
+++ inference.py
@@ -1,29 +1,32 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from transformers.generation import GenerationConfig
+from modelscope import (
+    snapshot_download, AutoModelForCausalLM, AutoTokenizer, GenerationConfig
+)
 import torch
+model_id = 'qwen/Qwen-VL-Chat'
+revision = 'v1.0.0'
+
+model_dir = "../7Bqw_vl_chat"
 torch.manual_seed(1234)
 
-# Note: The default behavior now has injection attack prevention off.
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen-VL-Chat", trust_remote_code=True)
-
+tokenizer = AutoTokenizer.from_pretrained(model_dir, trust_remote_code=True)
+if not hasattr(tokenizer, 'model_dir'):
+    tokenizer.model_dir = model_dir
 # use bf16
-# model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-VL-Chat", device_map="auto", trust_remote_code=True, bf16=True).eval()
+# model = AutoModelForCausalLM.from_pretrained(model_dir, device_map="auto", trust_remote_code=True, bf16=True).eval()
 # use fp16
-# model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-VL-Chat", device_map="auto", trust_remote_code=True, fp16=True).eval()
-# use cpu only
-# model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-VL-Chat", device_map="cpu", trust_remote_code=True).eval()
-# use cuda device
-model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-VL-Chat", device_map="cuda", trust_remote_code=True).eval()
+model = AutoModelForCausalLM.from_pretrained(model_dir, device_map="auto", trust_remote_code=True, fp16=True).eval()
+# use cpu
+# model = AutoModelForCausalLM.from_pretrained(model_dir, device_map="cpu", trust_remote_code=True).eval()
+# use auto
+model = AutoModelForCausalLM.from_pretrained(model_dir, device_map="auto", trust_remote_code=True).eval()
 
-# Specify hyperparameters for generation
-model.generation_config = GenerationConfig.from_pretrained("Qwen/Qwen-VL-Chat", trust_remote_code=True)
+# Specify hyperparameters for generation (No need to do this if you are using transformers>=4.32.0)
+# model.generation_config = GenerationConfig.from_pretrained(model_dir, trust_remote_code=True)
 
 # 1st dialogue turn
-query = tokenizer.from_list_format([
-    {'image': 'img/fridge.jpg'}, # Either a local path or an url
-    {'text': '这是什么?'},
-])
-response, history = model.chat(tokenizer, query=query, history=None)
+# Either a local path or an url between <img></img> tags.
+image_path = 'img/fridge.jpg'
+response, history = model.chat(tokenizer, query=f'<img>{image_path}</img>这是什么', history=None)
 print(response)
 
 
@@ -33,6 +36,6 @@ print(response)
 
 #image = tokenizer.draw_bbox_on_latest_picture(response, history)
 #if image:
-#  image.save('1.jpg')
+#  image.save('output_chat.jpg')
 #else:
 #  print("no box")
