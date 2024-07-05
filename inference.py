3c0112ba330484fd720096e9bf31985f115fd376
Bill Liu <billliu@BilldeMacBook-Pro.local>
2024-07-05 16:08:48 +0800

    inference

--- inference.py
+++ inference.py
@@ -20,19 +20,19 @@ model.generation_config = GenerationConfig.from_pretrained("Qwen/Qwen-VL-Chat", 
 
 # 1st dialogue turn
 query = tokenizer.from_list_format([
-    {'image': 'https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg'}, # Either a local path or an url
+    {'image': 'img/fridge.jpg'}, # Either a local path or an url
     {'text': '这是什么?'},
 ])
 response, history = model.chat(tokenizer, query=query, history=None)
 print(response)
-# 图中是一名女子在沙滩上和狗玩耍，旁边是一只拉布拉多犬，它们处于沙滩上。
+
 
 # 2nd dialogue turn
-response, history = model.chat(tokenizer, '框出图中击掌的位置', history=history)
+response, history = model.chat(tokenizer, '冰箱是什么颜色的', history=history)
 print(response)
-# <ref>击掌</ref><box>(536,509),(588,602)</box>
-image = tokenizer.draw_bbox_on_latest_picture(response, history)
-if image:
-  image.save('1.jpg')
-else:
-  print("no box")
+
+#image = tokenizer.draw_bbox_on_latest_picture(response, history)
+#if image:
+#  image.save('1.jpg')
+#else:
+#  print("no box")
