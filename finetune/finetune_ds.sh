6a8579fd4d83e8461a6d95dfce957295eb61ef52
Bill Liu <billliu@BilldeMacBook-Pro.local>
2024-07-05 17:34:28 +0800

    finetune

--- finetune/finetune_ds.sh
+++ finetune/finetune_ds.sh
@@ -8,7 +8,7 @@ NODE_RANK=0
 MASTER_ADDR=localhost
 MASTER_PORT=6001
 
-MODEL="/root/7Bqw-vl-chat/" #"Qwen/Qwen-VL-Chat"/"Qwen/Qwen-VL" # Set the path if you do not want to load from huggingface directly
+MODEL="/root/7Bqw_vl_chat" #"Qwen/Qwen-VL-Chat"/"Qwen/Qwen-VL" # Set the path if you do not want to load from huggingface directly
 # ATTENTION: specify the path to your training data, which should be a json file consisting of a list of conversations.
 # See the section for finetuning in README for more information.
 DATA="/root/Qwen-VL/data/a.json"
